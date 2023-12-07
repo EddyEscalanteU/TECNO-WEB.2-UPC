0b898d54dc0033e245f914ed6c99d3f6e630e652
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-12-06 21:51:48 -0400

    v3

--- DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_TRES/myApp3/src/app/app.component.ts
+++ DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_TRES/myApp3/src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-interface UserData {
-  id: number;
-  name: string;
-  email: string;
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  users: UserData[] = [
-    { id: 1, name: 'John Doe', email: 'john@example.com' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
-  ];
-
   title = 'myApp3';
 }
--- DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_TRES/myApp3/src/app/app.routes.ts
+++ DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_TRES/myApp3/src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { Pag1Component } from './pages/pag1/pag1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'pag1', component: Pag1Component },
+];
