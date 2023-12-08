fde8bfe1dc3ab8ce5eb9e6920eb1e84ceaca2310
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-12-08 19:33:17 -0400

    v1

---
+++ DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_DOS/myApp2/src/app/pages/pag4/pag4.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pag4',
+  standalone: true,
+  imports: [],
+  templateUrl: './pag4.component.html',
+  styleUrl: './pag4.component.scss'
+})
+export class Pag4Component {
+
+}
