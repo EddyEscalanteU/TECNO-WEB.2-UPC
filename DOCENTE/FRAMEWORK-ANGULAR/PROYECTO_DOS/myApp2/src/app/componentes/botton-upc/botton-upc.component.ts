6d68aa2e4be765ddfd5c5906501f5885e4fedc2a
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-12-11 20:53:22 -0400

    componentes timeline

--- DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_DOS/myApp2/src/app/componentes/botton-upc/botton-upc.component.ts
+++ DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_DOS/myApp2/src/app/componentes/botton-upc/botton-upc.component.ts
@@ -9,5 +9,4 @@ import { Component, Input } from '@angular/core';
 })
 export class BottonUpcComponent {
   @Input() TextoBoton: any = "0";
-  @Input() AnchoBoton: any = "100px";
 }
--- DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_DOS/myApp2/src/app/pages/pag2/pag2.component.ts
+++ DOCENTE/FRAMEWORK-ANGULAR/PROYECTO_DOS/myApp2/src/app/pages/pag2/pag2.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TimelineComponent } from '../../componentes/timeline/timeline.component';
 
 @Component({
   selector: 'app-pag2',
   standalone: true,
-  imports: [],
+  imports: [TimelineComponent],
   templateUrl: './pag2.component.html',
   styleUrl: './pag2.component.scss'
 })
