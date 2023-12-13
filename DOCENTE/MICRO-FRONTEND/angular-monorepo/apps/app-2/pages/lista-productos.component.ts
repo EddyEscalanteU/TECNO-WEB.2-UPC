7b42f12135f7cd1a6b9f867e7f0ac5d457f7b54b
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-12-12 20:04:43 -0400

    monorepo v1

---
+++ DOCENTE/MICRO-FRONTEND/angular-monorepo/apps/app-2/pages/lista-productos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-monorepo-lista-productos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lista-productos.component.html',
+  styleUrl: './lista-productos.component.css',
+})
+export class ListaProductosComponent {}
