d6b370ef581fe526aede471735ac4ce563e0cd88
VictorJimenez23 <153776233+VictorJimenez23@users.noreply.github.com>
2023-12-18 18:02:42 -0400

    componente boton al dia

--- VictorJimenez23/FRAMEWORK-ANGULAR/PROYECTO_2/myApp2/src/app/app.config.ts
+++ VictorJimenez23/FRAMEWORK-ANGULAR/PROYECTO_2/myApp2/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-//import { HttpClientModule } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
---
+++ VictorJimenez23/FRAMEWORK-ANGULAR/PROYECTO_2/myApp2/src/app/componentes/botton-upc/botton-upc.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-botton-upc',
+  standalone: true,
+  imports: [],
+  templateUrl: './botton-upc.component.html',
+  styleUrl: './botton-upc.component.scss'
+})
+export class BottonUpcComponent {
+@Input() TextoBoton: string="0";
+}
