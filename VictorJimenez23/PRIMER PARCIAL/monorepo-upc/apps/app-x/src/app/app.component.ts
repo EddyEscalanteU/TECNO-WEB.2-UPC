ed9fcbc545fba70660d74e9d752859694af0d311
VictorJimenez23 <153776233+VictorJimenez23@users.noreply.github.com>
2023-12-19 20:08:37 -0400

    pasando variables

--- VictorJimenez23/PRIMER PARCIAL/monorepo-upc/apps/app-x/src/app/app.component.ts
+++ VictorJimenez23/PRIMER PARCIAL/monorepo-upc/apps/app-x/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { CommonModule } from '@angular/common';
 import { MenuMegaComponent } from './libs/menu-mega/menu-mega.component';
 import{TablaFiltradoComponent} from'libs/tabla-filtrado/src/lib/tabla-filtrado/tabla-filtrado.component';
+import { UsuariosService } from '../service/usuarios/usuarios.service';
 @Component({
   standalone: true,
   imports: [MenuMegaComponent,CommonModule,NxWelcomeComponent, RouterOutlet, RouterLink, RouterLinkActive, RouterModule,TablaFiltradoComponent],
@@ -13,4 +14,17 @@ import{TablaFiltradoComponent} from'libs/tabla-filtrado/src/lib/tabla-filtrado/t
 })
 export class AppComponent {
   title = 'app-x';
+ public listadoUsuarios: any;
+  constructor(private usuariosService: UsuariosService,){
+    this.WebServiceGetUsuarios();
+  } 
+  
+  public WebServiceGetUsuarios(){
+    this.usuariosService.getUsuarios()
+      .subscribe(data => {
+        this.listadoUsuarios = data;
+        console.log(this.listadoUsuarios);
+      })
+  }
+
 }
--- VictorJimenez23/PRIMER PARCIAL/monorepo-upc/libs/tabla-filtrado/src/lib/tabla-filtrado/tabla-filtrado.component.ts
+++ VictorJimenez23/PRIMER PARCIAL/monorepo-upc/libs/tabla-filtrado/src/lib/tabla-filtrado/tabla-filtrado.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'monorepo-upc-tabla-filtrado',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink,RouterOutlet],
   templateUrl: './tabla-filtrado.component.html',
   styleUrl: './tabla-filtrado.component.css',
 })
 export class TablaFiltradoComponent {
-  
+  @Input() columna1:any;
+  @Input() columna2:string="null";
+  @Input() columna3:string="null";
 }
