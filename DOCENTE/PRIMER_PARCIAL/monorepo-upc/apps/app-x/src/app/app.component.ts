2f6bc0d4225a5fb1e97cd9610fb043582de141ed
“eescalante” <eescalante@geoautomotriz.com.bo>
2023-12-19 21:02:18 -0400

    Primer Parcial

---
+++ DOCENTE/PRIMER_PARCIAL/monorepo-upc/apps/app-x/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { MegaMenuComponent } from '@monorepo-upc/mega-menu';
+import { FilterTableComponent } from '@monorepo-upc/filter-table';
+import { UsersService } from './services/users/users.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, MegaMenuComponent, FilterTableComponent, CommonModule],
+  selector: 'monorepo-upc-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'app-x';
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public listaUsuarios: any;
+
+  public listaCol = ['IDE'];
+
+  constructor(private usersService: UsersService) {
+    this.WebServiceGetUsuarios();
+  }
+
+  public WebServiceGetUsuarios() {
+    this.usersService.GetUsuarios()
+      .subscribe(data => {
+        //console.log("data:", data);
+        this.listaUsuarios = data;
+        console.log("this.listaUsuarios:", this.listaUsuarios);
+      })
+
+  }
+
+
+}
