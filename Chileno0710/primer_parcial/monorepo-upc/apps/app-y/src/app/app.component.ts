736b49e2f92851c8dbe2e272d24275ab3866b258
Chileno0710 <153521647+Chileno0710@users.noreply.github.com>
2023-12-22 19:40:38 -0400

    practico5

---
+++ Chileno0710/primer_parcial/monorepo-upc/apps/app-y/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { MobileNavbarComponent } from '@monorepo-upc/mobile-navbar';
+import { FilterTableComponent } from '@monorepo-upc/filter-table';
+import { PostService } from './services/post/post.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, MobileNavbarComponent, FilterTableComponent, CommonModule],
+  selector: 'monorepo-upc-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'app-y';
+
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public listaPost: any;
+
+  public listaCol = ['IDE'];
+
+  constructor(private postService: PostService) {
+    this.WebServiceGetPost();
+  }
+
+  public WebServiceGetPost() {
+    this.postService.GetPost()
+      .subscribe(data => {
+        //console.log("data:", data);
+        this.listaPost = data;
+        console.log("this.listaPost:", this.listaPost);
+      })
+
+  }
+
+}
