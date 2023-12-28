8e901c1bb353560ec1858db82b0e634d586f7b44
EddyEscalanteU <eescalante@ucb.edu.bo>
2023-12-27 22:36:21 -0400

    EXAMEN FINAL IVETH

---
+++ icv-9/examen-final-upc/libs/card-view-horizontal/src/lib/card-view-horizontal/card-view-horizontal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'examen-final-upc-card-view-horizontal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-view-horizontal.component.html',
+  styleUrl: './card-view-horizontal.component.css',
+})
+export class CardViewHorizontalComponent {}
---
+++ icv-9/examen-final-upc/libs/card-view-vertical/src/lib/card-view-vertical/card-view-vertical.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'examen-final-upc-card-view-vertical',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-view-vertical.component.html',
+  styleUrl: './card-view-vertical.component.css',
+})
+export class CardViewVerticalComponent {}
